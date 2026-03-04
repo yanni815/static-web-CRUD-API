34ecbeacbc1b20dab171095af95d64ee15de303c
Ayanna Garrett <asgarrett@uncg.edu>
2026-03-04 13:27:25 -0500

    add Spring Boot project scaffold

---
+++ src/main/java/edu/uncg/character_api/CharacterApiApplication.java
@@ -0,0 +1,13 @@
+package edu.uncg.character_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharacterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharacterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/uncg/character_api/CharacterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uncg.character_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharacterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
