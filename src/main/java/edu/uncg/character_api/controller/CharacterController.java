fe6859f79151f702ec3c16a22456e78d27db84a2
Ayanna Garrett <asgarrett@uncg.edu>
2026-03-05 20:02:12 -0500

    Updated Repository file

--- src/main/java/edu/uncg/character_api/controller/CharacterController.java
+++ src/main/java/edu/uncg/character_api/controller/CharacterController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
-
 @RestController
 @RequestMapping("/characters")
 public class CharacterController {
--- src/main/java/edu/uncg/character_api/repository/CharacterRepository.java
+++ src/main/java/edu/uncg/character_api/repository/CharacterRepository.java
@@ -1,8 +1,40 @@
 package edu.uncg.character_api.repository;
 
 import edu.uncg.character_api.model.Character;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface CharacterRepository extends JpaRepository<Character, Long> {
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class CharacterRepository{
+    private List<Character> characters = new ArrayList<>();
+
+    public List<Character> getAllCharacters(){
+        return characters;
+    }
+
+    public Character getCharacterById(Long id){
+        for (Character c : characters){
+            if(c.getId().equals(id)){
+                return c;
+            }
+        }
+        return null;
+    }
+
+    public void updateCharacter(Long id, Character updatedCharacter){
+        for(int i = 0; i < characters.size(); i++){
+            if(characters.get(i).get(i).getId().equals(id)){
+                characters.set(i, updatedCharacter);
+                return;
+            }
+        }
+    }
+
+    public void deleteCharacter(Long id){
+        characters.removeIf(c -> c.getId().equals(id));
+    }
 
 
 }
