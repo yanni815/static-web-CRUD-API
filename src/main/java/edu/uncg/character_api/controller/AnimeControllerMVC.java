0fa49da054edf3c45bb1438bd63c02647f7188c8
Ayanna Garrett <asgarrett@uncg.edu>
2026-04-01 16:00:55 -0400

    added edit and delete

--- src/main/java/edu/uncg/character_api/controller/AnimeControllerMVC.java
+++ src/main/java/edu/uncg/character_api/controller/AnimeControllerMVC.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.service.registry.ImportHttpServices.Container;
@@ -41,4 +42,21 @@ public class AnimeControllerMVC {
         return "redirect:/characters";
     }
 
+    @GetMapping("/{id}/edit")
+    public String showUpdateForm(@PathVariable Long id, Model model){
+        model.addAttribute("character", animeCharacterService.getCharacterById(id));
+        return "character-update";
+    }
+
+    @PostMapping("/{id}")
+    public String updateCharacter(@PathVariable Long id, AnimeCharacter character) {
+        animeCharacterService.updateCharacter(id, character);
+        return "redirect:/characters/" + id;
+    }
+
+     @PostMapping("/{id}/delete")
+    public String deleteCharacter(@PathVariable Long id) {
+        animeCharacterService.deleteCharacter(id);
+        return "redirect:/characters";
+    }
 }
