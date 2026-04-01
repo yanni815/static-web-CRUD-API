a449826355bc52bb7e70a15fb22258e5dc1f845d
Ayanna Garrett <asgarrett@uncg.edu>
2026-04-01 15:08:42 -0400

    added create file

--- src/main/java/edu/uncg/character_api/controller/AnimeControllerMVC.java
+++ src/main/java/edu/uncg/character_api/controller/AnimeControllerMVC.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.service.registry.ImportHttpServices.Container;
 
+import edu.uncg.character_api.model.AnimeCharacter;
 import edu.uncg.character_api.service.AnimeCharacterService;
 
 @Controller
@@ -27,4 +29,16 @@ public class AnimeControllerMVC {
         model.addAttribute("character", animeCharacterService.getCharacterById(id));
         return "character-details";
     }
+
+    @GetMapping("/create")
+    public String showCreateForm(){
+        return "character-create";
+    }
+
+    @PostMapping
+    public String createCharacter(AnimeCharacter character){
+        animeCharacterService.createCharacter(character);
+        return "redirect:/characters";
+    }
+
 }
---
+++ src/main/resources/templates/character-create.ftl
@@ -0,0 +1,9 @@
+<h1>Add Character</h1>
+
+<form action="/characters" method="post">
+<input type="text" name="name" placeholder="Character Name" />
+<input type="text" name="anime" placeholder="Anime" />
+
+<button type="submit">Create</button>
+</form>
+<a href="/characters">Back</a>
