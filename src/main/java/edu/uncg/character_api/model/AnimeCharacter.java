1b9b63d85b7df60b10310ef3ee28d51afef60e62
Ayanna Garrett <asgarrett@uncg.edu>
2026-04-01 19:03:57 -0400

    added generated value

--- src/main/java/edu/uncg/character_api/model/AnimeCharacter.java
+++ src/main/java/edu/uncg/character_api/model/AnimeCharacter.java
@@ -1,11 +1,14 @@
 package edu.uncg.character_api.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class AnimeCharacter{
     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String anime;
